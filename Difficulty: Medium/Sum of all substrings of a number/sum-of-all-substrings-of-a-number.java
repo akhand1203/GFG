5dc559517d95de4858acf4a8f8169b8e5bace306
AKHAND PRATAP SINGH <123093534+akhand1203@users.noreply.github.com>
2026-06-28 10:58:21 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Sum of all substrings of a number/sum-of-all-substrings-of-a-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public static int sumSubstrings(String s) {
+        // code here
+        int sum=0;
+        for (int i=0;i<s.length();i++){
+            for(int j=i;j<s.length();j++){
+                String str=s.substring(i,j+1);
+                sum+=Integer.parseInt(str);
+            }
+        }
+        return sum;
+    }
+}
